ee766df8d30fe7001342c7acbf977173ead2d341
Karthik016996148 <145539708+Karthik016996148@users.noreply.github.com>
2023-09-20 23:46:26 -0700

    Update DATA200HW3NK.py

--- DATA200HW3NK.py
+++ DATA200HW3NK.py
@@ -1,14 +1,6 @@
-import matplotlib.pyplot as plt
+import plotly.express as ps
 import streamlit as st
 import pandas as pd
-data = pd.read_csv(r"C:\Users\Karthik\Desktop\SEM 1\DATA 200\ASsignments\toy_dataset.csv")
-df = pd.DataFrame(data)
-x=list(df.iloc[:, 1])
-y=list(df.iloc[:, 4])
-plt.bar(x, y, width=0.4)
-plt.xticks(rotation=45, ha='right')
-plt.title("SALARY BASED ON LOCATION")
-plt.xlabel("CITY")
-plt.ylabel("INCOME")
-fig=plt.show()
-st.pyplot(fig)
+df = pd.read_csv(Fish.csv)
+fig = px.bar(df, x='Species',y='Weight')
+st.plotly_chart(fig)
