c04122c1ddf6e82dbe8dd3ddf11e8c27822f06f6
Karthik016996148 <145539708+Karthik016996148@users.noreply.github.com>
2023-09-21 12:22:49 -0700

    Update DATA200HW3NK.py

--- DATA200HW3NK.py
+++ DATA200HW3NK.py
@@ -2,5 +2,5 @@ import plotly.express as px
 import streamlit as st
 import pandas as pd
 df = pd.read_csv("Fish.csv")
-fig = px.bar(df, x='Species',y='Weight', tittle='Fish')
+fig = px.bar(df, x='Species',y='Weight', title='Fish')
 st.plotly_chart(fig)
