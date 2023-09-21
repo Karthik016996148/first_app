1873e3e906058c0c829972ff1ea401150b801a74
Karthik016996148 <145539708+Karthik016996148@users.noreply.github.com>
2023-09-21 00:59:47 -0700

    Add files via upload

---
+++ DATA200HW3NK.py
@@ -0,0 +1,6 @@
+import plotly.express as px
+import streamlit as st
+import pandas as pd
+df = pd.read_csv(r"C:\Users\Karthik\Desktop\SEM 1\DATA 200\ASsignments\Fish.csv")
+fig = px.bar(df, x='Species',y='Weight')
+st.plotly_chart(fig)
