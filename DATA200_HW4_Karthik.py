42a54cd712358ddbab84bd0e337275e8112d8e12
Karthik016996148 <145539708+Karthik016996148@users.noreply.github.com>
2023-09-28 13:54:04 -0700

    Update DATA200_HW4_Karthik.py

--- DATA200_HW4_Karthik.py
+++ DATA200_HW4_Karthik.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import plotly.express as px
+import matplotlib.pyplot as plt
 import pandas as pd
 data = pd.read_csv("toy_dataset.csv")
 df = pd.DataFrame(data)
@@ -7,9 +7,9 @@ result = df.head(25)
 st.write(result)
 x=list(df.iloc[:, 1])
 y=list(df.iloc[:, 4])
-fig = px.bar(x, y, width=0.4)
-fig.update_xaxes(tickangle=45, tickvals=x, ticktext=x)
-px.title("SALARY BASED ON LOCATION")
-px.xlabel("CITY")
-px.ylabel("INCOME")
-fig.show() 
+plt.bar(x, y, width=0.4)
+plt.xticks(rotation=45, ha='right')
+plt.title("SALARY BASED ON LOCATION")
+plt.xlabel("CITY")
+plt.ylabel("INCOME")
+st.pyplot(plt.gcf()) 
