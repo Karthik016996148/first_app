2a56c42b232124362055fe891463c54869754443
Karthik016996148 <145539708+Karthik016996148@users.noreply.github.com>
2023-09-28 13:02:14 -0700

    Add files via upload

---
+++ DATA200_HW4_Karthik.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
+data = pd.read_csv("toy_dataset.csv")
+df = pd.DataFrame(data)
+result = df.head(25)
+st.write(result)
+x=list(df.iloc[:, 1])
+y=list(df.iloc[:, 4])
+plt.bar(x, y, width=0.4)
+plt.xticks(rotation=45, ha='right')
+plt.title("SALARY BASED ON LOCATION")
+plt.xlabel("CITY")
+plt.ylabel("INCOME")
+st.pyplot(plt.gcf())
