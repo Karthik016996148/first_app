fe3a36ad1e905941b03eb2c70112fffd5288ddaa
Karthik016996148 <145539708+Karthik016996148@users.noreply.github.com>
2023-09-28 14:28:04 -0700

    Update DATA200_HW4_Karthik.py

--- DATA200_HW4_Karthik.py
+++ DATA200_HW4_Karthik.py
@@ -1,15 +1,11 @@
 import streamlit as st
-import matplotlib.pyplot as plt
 import pandas as pd
-data = pd.read_csv("toy_dataset.csv")
-df = pd.DataFrame(data)
-result = df.head(25)
-st.write(result)
-x=list(df.iloc[:, 1])
-y=list(df.iloc[:, 4])
-plt.bar(x, y, width=0.4)
-plt.xticks(rotation=45, ha='right')
-plt.title("SALARY BASED ON LOCATION")
-plt.xlabel("CITY")
-plt.ylabel("INCOME")
-st.pyplot(plt.gcf()) 
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+})) 
+df = pd.read_csv("Fish.csv")
+fig = px.bar(df, x='Species',y='Weight', title='WEIGHTS OF DIFFERENT TYPES OF FISH')
+st.plotly_chart(fig)
