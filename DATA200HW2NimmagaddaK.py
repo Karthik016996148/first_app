9a2a760aee316655e3d1f0a58b10fdb81be84ea7
Karthik016996148 <145539708+Karthik016996148@users.noreply.github.com>
2023-10-08 18:54:00 -0700

    Update DATA200HW2NimmagaddaK.py

--- DATA200HW2NimmagaddaK.py
+++ DATA200HW2NimmagaddaK.py
@@ -2,9 +2,6 @@ import streamlit as st
 import pandas as pd
 
 st.write("Here's our first attempt at using data to create a table:")
-st.sidebar.header('Interactive Controls')
-selected_variable = st.sidebar.selectbox('Select a variable:', ('first column', 'second column'))
-slider_value = st.sidebar.slider('Select a range:', min_value=0, max_value=100, value=(0, 100))
 st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
